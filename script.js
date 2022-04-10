6843425d91037c9c1c48af414348af6239f38c3f
Elmosh32 <mosheelazar81@gmail.com>
2022-04-11 01:37:52 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function delay (URL) {
+  setTimeout(function () {
+      window.open(URL);
+  }, 400);
+}
+
+function delayLogo (URL) {
+  setTimeout(function () {
+      window.open(URL, "_self");
+  }, 1100);
+}
